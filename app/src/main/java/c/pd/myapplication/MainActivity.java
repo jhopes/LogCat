2201c7aeca737ff2aeaf14da692dd8072b14fdca
jhopes <jhopes.p@gmail.com>
2018-08-20 15:27:22 -0500

    Intent

--- app/src/main/java/c/pd/myapplication/MainActivity.java
+++ app/src/main/java/c/pd/myapplication/MainActivity.java
@@ -1,15 +1,29 @@
 package c.pd.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-String TAG;
+    Button btnlogin;
+    String TAG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG,"Soy información");
+        btnlogin = (Button) findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                intent.putExtra("x","hola login");
+                startActivity(intent);
+            }
+        });
     }
+
 }
